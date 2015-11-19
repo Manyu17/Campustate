5c6cc4884913269b3caef20140778c64bdb7f95f
UtopiaScript <qiufengmanyu@gmail.com>
2015-11-19 20:35:03 +0800

    11-19

---
+++ js/campustate.js
@@ -0,0 +1,11 @@
+// var metaEl = doc.createElement('meta');
+// var scale = isRetina ? 0.5:1;
+// metaEl.setAttribute('name', 'viewport');
+// metaEl.setAttribute('content', 'initial-scale=' + scale + ', maximum-scale=' + scale + ', minimum-scale=' + scale + ', user-scalable=no');
+// if (docEl.firstElementChild) {
+//     document.documentElement.firstElementChild.appendChild(metaEl);
+// } else {
+//     var wrap = doc.createElement('div');
+//     wrap.appendChild(metaEl);
+//     documen.write(wrap.innerHTML);
+// }
